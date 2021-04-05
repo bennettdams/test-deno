0acd1be5b10981247aca8e22161fb9946debcd61
Bennett <dams.bennett@gmail.com>
2021-04-05 17:40:08 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-setInterval(() => console.log(`Test log ${Date.now()}`), 5 * 1000)
+addEventListener("fetch", (event) => {
+  // Get the value of the header named `x-forwarded-for`.
+  // The value can either be a IPv4 or IPv6 address.
+  const ip = event.request.headers.get("x-forwarded-for");
+  const response = new Response(`Your IP address is <b>${ip}</b>`, {
+    headers: { "content-type": "text/html" },
+  });
+  event.respondWith(response);
+});
